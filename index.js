dc5909c61afad7a7c34f0b7dc9a0fe85b0cc2856
Faisal Herawy <faisalherawy@icloud.com>
2023-11-08 12:08:55 +0300

    built the two main functions of the assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const options = ["Rock", "Paper", "Scissors"]
+let playerSelection = prompt("Choose: Rock, Paper, Scissors")
+function getComputerChoice () {
+    let computerChoice = options[Math.floor(Math.random()*options.length)];
+    return computerChoice
+}
+
+function playSingleRound (playerSelection, computerSelection) {
+   
+
+     
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        console.log("It's a tie, you both chose Rock")
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You lose, you chose Rock, Computer chose Paper")
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("You win, you chose Rock, Computer chose Scissors")
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You win, you chose Paper, Computer chose Rock")
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("You lose, you chose Scissors, Computer chose Rock")
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        console.log("It's a tie, you both chose Paper")
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("You lose, you chose Paper, Computer chose Scissors")
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("You win, you chose Scissors, Computer chose Paper")
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        console.log("It's a tie, you both chose Scissors")
+    } else {
+        console.log("Error")
+    }
+}
+
+playSingleRound(playerSelection, getComputerChoice())
